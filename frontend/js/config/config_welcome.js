50617deca983ace4602ba122a120ac074cf73394
yuanhuic <yuanhuic@zoosk.com>
2014-08-06 23:19:59 -0700

    add externs

--- frontend/js/config/config_welcome.js
+++ frontend/js/config/config_welcome.js
@@ -5,6 +5,9 @@
         "@PROJECT_DIR@/src",
         "@PROJECT_DIR@/closure/third_party"
     ],
+    "externs": [
+     "@PROJECT_DIR@/externs/angular.js"
+     ],
   "closure-library": "@PROJECT_DIR@/closure/closure/goog",
   "module-output-path": "@PROJECT_DIR@/build/module_%s.js",
     "modules": {
