cb162232d8909bfcb75557f92fac737864d64f07
reshmasoap <jahan_z3b@hotmail.com>
2015-07-22 00:40:02 -0300

    update

--- backend/node/src/config/database.js
+++ backend/node/src/config/database.js
@@ -2,5 +2,6 @@
 module.exports = {
 
 	'url' : 'mongodb://localhost/jamoutDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://jamoutTv:JamoutTv54@ds033699.mongolab.com:33699/jamoutdb' // mongolab
 
 };
