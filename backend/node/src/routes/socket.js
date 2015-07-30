f53e792a122568cec930c8e420eb694606aceb00
J <jahan_z3b@hotmail.com>
2015-07-30 15:37:48 -0300

    fixed socket.io userids

--- backend/node/src/routes/socket.js
+++ backend/node/src/routes/socket.js
@@ -43,14 +43,15 @@ exports.start= function (io) {
     /** @const */
     var room = rooms[currentRoom];
 
-    if (room) 
-    {
-      if (room.length > 2) 
-      {
-        socket.emit('peer.limit', {error: "The room you're trying to reach is at full capacity :( . Please try again later <3"});
-        return;
-      } 
-    }
+
+    // if (room) 
+    // {
+    //   if (room.length > 2) 
+    //   {
+    //     socket.emit('peer.limit', {error: "The room you're trying to reach is at full capacity :( . Please try again later <3"});
+    //     return;
+    //   } 
+    // }
 
       if(!data) 
           {
@@ -63,19 +64,24 @@ exports.start= function (io) {
               socket.emit('peer.limit', {error: "This stream has expired"});
               return;
             }
+                 userIds[currentRoom] += 1;
+                 id = userIds[currentRoom];
 
-             userIds[currentRoom] += 1;
-             id = userIds[currentRoom];
-
-             fn(currentRoom, id);
-             console.log(userIds[currentRoom]);
-                
-               room.forEach(function (s) 
-                {
-                  s.emit('peer.connected', { id: id });
-                });
-                  room[id] = socket;
-                  console.log('Peer connected to room', currentRoom, 'with #', id);
+              if (userIds[currentRoom] < 2) {
+                 fn(currentRoom, id);
+                 console.log(userIds[currentRoom]);
+                    
+                   room.forEach(function (s) 
+                    {
+                      s.emit('peer.connected', { id: id });
+                    });
+                      room[id] = socket;
+                      console.log('Peer connected to room', currentRoom, 'with #', id);
+                } else {
+                    console.log('Room is at full capacity for', userIds[currentRoom]);
+                    socket.emit('peer.limit', {error: "The room you're trying to reach is at full capacity :( . Please try again later <3"});
+                    return;
+                }
           }
 
   });
@@ -93,10 +99,14 @@ exports.start= function (io) {
 
   socket.on('disconnect', function () {
     console.log('Disconnecting Peer');
+    console.log('userid', userIds[currentRoom]);
     console.log('id', id);
     if(!currentRoom || !rooms[currentRoom]) {
       return;
     }
+    userIds[currentRoom] -= 1;
+    console.log('userid after', userIds[currentRoom]);
+
     delete rooms[currentRoom][rooms[currentRoom].indexOf(socket)];
     rooms[currentRoom].forEach(function (socket) {
       if (socket) {
