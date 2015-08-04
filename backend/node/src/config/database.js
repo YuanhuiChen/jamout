5d8732be33dea69e99e848334f2f77df046e5035
J <jahan_z3b@hotmail.com>
2015-08-04 13:20:41 -0300

    git rm deleted files and changed to local db

--- backend/node/src/config/database.js
+++ backend/node/src/config/database.js
@@ -1,7 +1,7 @@
 // config/database.js
 module.exports = {
 
-	//'url' : 'mongodb://localhost/jamoutDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-	'url' : 'mongodb://jamoutTv:JamoutTv54@ds033699.mongolab.com:33699/jamoutdb' // mongolab
+	'url' : 'mongodb://localhost/jamoutDB' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+	//'url' : 'mongodb://jamoutTv:JamoutTv54@ds033699.mongolab.com:33699/jamoutdb' // mongolab
 
 };
